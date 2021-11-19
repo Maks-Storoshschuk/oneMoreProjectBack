81ddfc480c3ed68e467a43044dff02263d1e970e
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-11-19 10:57:25 +0200

    start

--- services/jwt.service.js
+++ services/jwt.service.js
@@ -9,7 +9,7 @@ const {tokenTypeEnum} = require('../config');
 
 module.exports = {
     generateTokenPair: () => {
-        const access_token = jwt.sign({}, JWT_ACCESS_SECRET, {expiresIn: '1d'});
+        const access_token = jwt.sign({}, JWT_ACCESS_SECRET, {expiresIn: '15m'});
         const refresh_token = jwt.sign({}, JWT_REFRESH_SECRET, {expiresIn: '30d'});
 
         return {
